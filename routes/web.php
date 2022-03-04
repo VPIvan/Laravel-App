62b8faf45f19440b09202337f69107a7590a096c
Ivan Penaloza <ivanpenaloza11@gmail.com>
2022-03-03 21:26:05 -0800

    edited for the error on deployment to heroku for view board not found

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
+Route::get('/todos', function () {
     return view('todos');
 });
 
-Route::get('/', function () {
-    return view('calendar');
+Route::get('/calendar', function () {
+    return view('schedule');
 });
 
-Route::get('/', function () {
+Route::get('/board', function () {
     return view('board');
 });
