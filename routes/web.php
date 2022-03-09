ea669f8bb92ece5f875c9f547639773819e52954
Ivan <ivanpenaloza11@gmail.com>
2022-03-09 09:23:29 -0800

    made route get events-feed into a doubly nested array

--- routes/web.php
+++ routes/web.php
@@ -30,18 +30,24 @@ Route::get('/board', function () {
 });
 
 Route::get('events-feed/', function(){
-    return '[
-        {
-          "title": "CSE4500 Class",
-          "start": "2022-02-23T17:30:00",
-          "end": "2022-02-23T18:45:00"
-        },
-        {
-          "title": "CSE4500 Class",
-          "start": "2022-02-28T17:30:00",
-          "end": "2022-02-28T18:45:00"
-        }   
-      ]';
+    $data = array(
+        array(
+          'title' => 'CSE4500 Class',
+          'start' => "2022-02-23T17:30:00",
+          'end' => '2022-02-23T18:45:00'
+        ),
+        array (
+          'title' => 'CSE4500 Class',
+          'start' => '2022-02-28T17:30:00',
+          'end' => '2022-02-28T18:45:00'
+        )   
+    );
+});
+
+Route::get('/db-migrate', function()
+{
+    Artisan::call('migrate');
+    echo Artisan::output();
 });
 
 Route::get('/db-test', function () {
