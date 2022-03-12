df9a88ebf8f9376775b267263d2620c8eca1fa25
Ivan Penaloza <ivanpenaloza11@gmail.com>
2022-03-11 20:41:58 -0800

    changes

--- database/migrations/2022_03_11_161910_create_events_table.php
+++ database/migrations/2022_03_11_161910_create_events_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->dateTime('begin');
             $table->dateTime('finish');
-            $table->timestamp();
+            $table->timestamps();
         });
     }
 
