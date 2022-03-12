699e7bd0e3eba9e192104f4cdc624788f9e0bb70
Ivan Penaloza <ivanpenaloza11@gmail.com>
2022-03-11 20:17:11 -0800

    changes

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -15,3 +15,23 @@
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
+
+
+
+@extends('adminlte::page')
+
+@section('title', 'To Do List')
+
+@section('content_header')
+    <h1>Calender Events</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('calenderevent.store') }}" >
+    @csrf
+    <x-adminlte-input name="title" label="Title" />
+    <x-adminlte-input name="begin" type="datetime-local" label="Start" />
+    <x-adminlte-input name="finish" type="datetime-local" label="Finish" />
+    <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
